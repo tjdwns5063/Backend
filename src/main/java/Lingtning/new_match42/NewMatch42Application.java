4e6029073118fa91267d72686a0e305ab72aa05e
feedbaek <nicek789@naver.com>
2023-08-21 17:19:00 +0900

    Init

---
+++ src/main/java/Lingtning/new_match42/NewMatch42Application.java
@@ -0,0 +1,13 @@
+package Lingtning.new_match42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMatch42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMatch42Application.class, args);
+	}
+
+}
---
+++ src/test/java/Lingtning/new_match42/NewMatch42ApplicationTests.java
@@ -0,0 +1,13 @@
+package Lingtning.new_match42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMatch42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
