a67dd74459a57a257ec4078d66746441f0840960
feedbaek <nicek789@naver.com>
2023-09-01 17:49:58 +0900

    Feat: 앱 종료시에도 알림 수신

--- src/main/java/Lingtning/new_match42/service/FCMService.java
+++ src/main/java/Lingtning/new_match42/service/FCMService.java
@@ -2,10 +2,7 @@ package Lingtning.new_match42.service;
 
 import Lingtning.new_match42.entity.User;
 import Lingtning.new_match42.repository.UserRepository;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,7 +21,7 @@ public class FCMService {
     public void sendChatMessage(User user, String message) {
         String token = user.getFcmToken();
         if (token == null) {
-            throw new ResponseStatusException(BAD_REQUEST, "FCM 토큰이 없습니다.");
+            throw new ResponseStatusException(BAD_REQUEST, "알림을 받을 대상에게 FCM 토큰이 없습니다.");
         }
         Notification notification = Notification.builder()
                 .setTitle("매치42")
@@ -33,22 +30,36 @@ public class FCMService {
 
         Message fcmMessage = Message.builder()
                 .setNotification(notification)
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setTtl(3600 * 1000)
+                        .setPriority(AndroidConfig.Priority.HIGH)
+                        .build())
+                .setApnsConfig(ApnsConfig.builder()
+                        .setAps(Aps.builder()
+                                .setContentAvailable(true)
+                                .build())
+                        .putHeader("apns-push-type", "background")
+                        .putHeader("apns-priority", "5")
+                        .putHeader("apns-topic", "com.lingtning.match42")
+                        .build())
                 .setToken(token)
                 .build();
 
         try {
             firebaseMessaging.send(fcmMessage);
         } catch (FirebaseMessagingException e) {
-            throw new ResponseStatusException(INTERNAL_SERVER_ERROR, "FCM 메시지 전송에 실패했습니다.");
+            throw new ResponseStatusException(BAD_REQUEST, "FCM 메시지 전송에 실패했습니다. " + e.getMessage());
         }
         throw new ResponseStatusException(OK, "FCM 메시지 전송에 성공했습니다.");
     }
 
     public void subscribeToken(User user, String token) {
-        if (user.getFcmToken() != null) {
-            throw new ResponseStatusException(BAD_REQUEST, "이미 등록된 FCM 토큰이 있습니다.");
-        }
         user.setFcmToken(token);
+        try {
+            sendChatMessage(user, "FCM 토큰 등록에 성공했습니다.");
+        } catch (Exception e) {
+            throw new ResponseStatusException(BAD_REQUEST, "유효하지 않은 토큰입니다.");
+        }
         try {
             userRepository.save(user);
         } catch (Exception e) {
