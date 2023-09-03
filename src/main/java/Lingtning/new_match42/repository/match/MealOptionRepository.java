9cd4a20079c3539d1759ac27bb6711b1b0e371b9
feedbaek <nicek789@naver.com>
2023-09-03 20:50:53 +0900

    Feat: 차단유저 매칭 반영 금지

--- src/main/java/Lingtning/new_match42/repository/match/MealOptionRepository.java
+++ src/main/java/Lingtning/new_match42/repository/match/MealOptionRepository.java
@@ -4,9 +4,12 @@ import Lingtning.new_match42.entity.match.MealOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MealOptionRepository extends JpaRepository<MealOption, Long> {
     Optional<MealOption> findTopByCapacityAndMenu(Integer capacity, String menu);
+
+    List<MealOption> findByCapacityAndMenu(Integer capacity, String menu);
 }
--- src/main/java/Lingtning/new_match42/repository/match/SubjectOptionRepository.java
+++ src/main/java/Lingtning/new_match42/repository/match/SubjectOptionRepository.java
@@ -3,8 +3,11 @@ package Lingtning.new_match42.repository.match;
 import Lingtning.new_match42.entity.match.SubjectOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubjectOptionRepository extends JpaRepository<SubjectOption, Long> {
     Optional<SubjectOption> findTopByCapacityAndProject(Integer capacity, String project);
+
+    List<SubjectOption> findByCapacityAndProject(Integer capacity, String project);
 }
