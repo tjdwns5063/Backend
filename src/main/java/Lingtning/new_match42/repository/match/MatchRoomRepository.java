09c9caea2ae7b2a7d0f1460e115d903e74f6ffe9
seongjunkim <tjdwns5063@naver.com>
2024-01-24 17:09:47 +0900

    remove: 사용하지 않는 파일 삭제

--- src/main/java/Lingtning/new_match42/repository/match/MatchRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package Lingtning.new_match42.repository.match;
-
-import Lingtning.new_match42.entity.match.MatchRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MatchRoomRepository extends JpaRepository<MatchRoom, Long> {
-}
